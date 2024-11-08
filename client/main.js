b881a364b9e74c702fff2dc1f03282c8855fc5b7
raphael <raphael.moynat@gmail.com>
2024-11-08 14:14:55 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,17 @@
+const socket = io("ws://localhost:8080")
+const btn = document.querySelector('button')
+
+socket.on('message', (message) => {
+    console.log(message)
+    const line = document.createElement('li')
+    line.innerHTML = message.author + ' : ' + message.content
+    document.querySelector('ul').appendChild(line)
+
+})
+
+btn.addEventListener('click', () => {
+    const messageToSend = document.querySelector('input').value
+    socket.emit('message', {
+        content : messageToSend,
+    })
+})
---
+++ server/index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('serveur socket.io en fonctionnement');
+
+})
+
+const io = socketIo(server, {
+    transports: ['websocket', 'polling'],
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+
+})
+
+io.on('connection', (socket) => {
+    console.log('New user connect to the server');
+
+    socket.on('message', (message) => {
+        console.log('Received message from ' + socket.id);
+        console.log(message.content);
+
+        try{
+            io.emit('message',{
+                author : socket.id,
+                content: message.content,
+            });
+        }catch (e) {
+            console.log(e);
+        }finally {
+            console.log(io.sockets.sockets.size);
+        }
+    })
+
+})
+
+
+
+    server.listen(8080, ()=>{
+        console.log('Server listening on port 8080');
+    });
